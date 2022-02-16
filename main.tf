7e28aa5d98c64dc19e75e72e6b28529ed34225e4
Kevin Lefevre <kevin@particule.io>
2022-02-16 11:32:00 +0100

    feat: replace lookup with try
    Signed-off-by: Kevin Lefevre <kevin@particule.io>

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # Main module resources
 
 resource "tls_private_key" "ca" {
-  algorithm   = lookup(var.ca, "algorithm", "ECDSA")
-  ecdsa_curve = lookup(var.ca, "algorithm", "ECDSA") == "ECDSA" ? try(var.ca.ecdsa_curve, "P384") : null
-  rsa_bits    = lookup(var.ca, "algorithm", "ECDSA") == "RSA" ? try(var.ca.rsa_bits, 4096) : null
+  algorithm   = try(var.ca.algorithm, "ECDSA")
+  ecdsa_curve = try(var.ca.algorithm, "ECDSA") == "ECDSA" ? try(var.ca.ecdsa_curve, "P384") : null
+  rsa_bits    = try(var.ca.algorithm, "ECDSA") == "RSA" ? try(var.ca.rsa_bits, 4096) : null
 }
 
 resource "tls_self_signed_cert" "ca" {
@@ -12,21 +12,21 @@ resource "tls_self_signed_cert" "ca" {
   is_ca_certificate = true
 
   subject {
-    common_name         = lookup(var.ca.subject, "common_name", null)
-    organization        = lookup(var.ca.subject, "organization", null)
-    organizational_unit = lookup(var.ca.subject, "organizational_unit", null)
-    street_address      = lookup(var.ca.subject, "street_address", null)
-    locality            = lookup(var.ca.subject, "locality", null)
-    province            = lookup(var.ca.subject, "province", null)
-    country             = lookup(var.ca.subject, "country", null)
-    postal_code         = lookup(var.ca.subject, "postal_code", null)
-    serial_number       = lookup(var.ca.subject, "serial_number", null)
+    common_name         = try(var.ca.subject.common_name, null)
+    organization        = try(var.ca.subject.organization, null)
+    organizational_unit = try(var.ca.subject.organizational_unit, null)
+    street_address      = try(var.ca.subject.street_address, null)
+    locality            = try(var.ca.subject.locality, null)
+    province            = try(var.ca.subject.province, null)
+    country             = try(var.ca.subject.country, null)
+    postal_code         = try(var.ca.subject.postal_code, null)
+    serial_number       = try(var.ca.subject.serial_number, null)
   }
 
-  validity_period_hours = lookup(var.ca, "validity_period_hours", 87600)
-  early_renewal_hours   = lookup(var.ca, "early_renewal_hours", 78840)
+  validity_period_hours = try(var.ca.validity_period_hours, 87600)
+  early_renewal_hours   = try(var.ca.early_renewal_hours, 78840)
 
-  allowed_uses = lookup(var.ca, "allowed_uses", [
+  allowed_uses = try(var.ca.allowed_uses, [
     "cert_signing",
     "crl_signing",
     "code_signing",
@@ -39,9 +39,9 @@ resource "tls_self_signed_cert" "ca" {
 
 resource "tls_private_key" "certificate" {
   for_each    = var.certificates
-  algorithm   = lookup(each.value, "algorithm", "ECDSA")
-  ecdsa_curve = lookup(each.value, "algorithm", "ECDSA") == "ECDSA" ? lookup(each.value, "ecdsa_curve", "P384") : null
-  rsa_bits    = lookup(each.value, "algorithm", "ECDSA") == "RSA" ? lookup(each.value, "rsa_bits", 4096) : null
+  algorithm   = try(each.value.algorithm, "ECDSA")
+  ecdsa_curve = try(each.value.algorithm, "ECDSA") == "ECDSA" ? try(each.value.ecdsa_curve, "P384") : null
+  rsa_bits    = try(each.value.algorithm, "ECDSA") == "RSA" ? try(each.value.rsa_bits, 4096) : null
 }
 
 resource "tls_cert_request" "certificate" {
@@ -50,20 +50,20 @@ resource "tls_cert_request" "certificate" {
   private_key_pem = tls_private_key.certificate[each.key].private_key_pem
 
   subject {
-    common_name         = lookup(each.value.subject, "common_name", null)
-    organization        = lookup(each.value.subject, "organization", null)
-    organizational_unit = lookup(each.value.subject, "organizational_unit", null)
-    street_address      = lookup(each.value.subject, "street_address", null)
-    locality            = lookup(each.value.subject, "locality", null)
-    province            = lookup(each.value.subject, "province", null)
-    country             = lookup(each.value.subject, "country", null)
-    postal_code         = lookup(each.value.subject, "postal_code", null)
-    serial_number       = lookup(each.value.subject, "serial_number", null)
+    common_name         = try(each.value.subject.common_name, null)
+    organization        = try(each.value.subject.organization, null)
+    organizational_unit = try(each.value.subject.organizational_unit, null)
+    street_address      = try(each.value.subject.street_address, null)
+    locality            = try(each.value.subject.locality, null)
+    province            = try(each.value.subject.province, null)
+    country             = try(each.value.subject.country, null)
+    postal_code         = try(each.value.subject.postal_code, null)
+    serial_number       = try(each.value.subject.serial_number, null)
   }
 
-  dns_names    = lookup(each.value, "dns_names", [])
-  ip_addresses = lookup(each.value, "ip_addresses", [])
-  uris         = lookup(each.value, "uris", [])
+  dns_names    = try(each.value.dns_names, [])
+  ip_addresses = try(each.value.ip_addresses, [])
+  uris         = try(each.value.uris, [])
 }
 
 resource "tls_locally_signed_cert" "certificate" {
@@ -73,10 +73,10 @@ resource "tls_locally_signed_cert" "certificate" {
   ca_private_key_pem = tls_private_key.ca.private_key_pem
   ca_cert_pem        = tls_self_signed_cert.ca.cert_pem
 
-  validity_period_hours = lookup(each.value, "validity_period_hours", 8740)
-  early_renewal_hours   = lookup(each.value, "early_renewal_hours", 8040)
+  validity_period_hours = try(each.value.validity_period_hours, 8740)
+  early_renewal_hours   = try(each.value.early_renewal_hours, 8040)
 
-  allowed_uses = lookup(each.value, "allowed_uses", [])
+  allowed_uses = try(each.value.allowed_uses, [])
 }
 
 resource "pkcs12_from_pem" "certificate" {
