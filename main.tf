275b0fccbd355e594c6337a452bd65d5f65ba5b5
Kevin Lefevre <kevin@particule.io>
2022-07-05 22:52:28 +0200

    fix remove deprecated key_algorithm
    Signed-off-by: Kevin Lefevre <kevin@particule.io>

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ resource "tls_private_key" "ca" {
 }
 
 resource "tls_self_signed_cert" "ca" {
-  key_algorithm     = tls_private_key.ca.algorithm
   private_key_pem   = tls_private_key.ca.private_key_pem
   is_ca_certificate = true
 
